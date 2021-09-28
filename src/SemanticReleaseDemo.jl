6ef908eae72d3e249ee3de64dc4f78328fb525c6
Jaan Tollander de Balsch <jaan@hey.com>
2021-09-28 12:25:28 +0300

    chore: initial commit

---
+++ src/SemanticReleaseDemo.jl
@@ -0,0 +1,5 @@
+module SemanticReleaseDemo
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SemanticReleaseDemo
+using Test
+
+@testset "SemanticReleaseDemo.jl" begin
+    # Write your tests here.
+end
